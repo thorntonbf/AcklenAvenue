e1a986dd2cba50c4eeeec24be93e9ace83619f24
Bryan Thornton <bthornton@netreaction.com>
2015-02-27 10:51:46 -0600

    Push to a public repo
    Initial push to a public repository

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+$("#slideshow").css("overflow", "hidden");
+
+$("ul#slides").cycle({
+	fx: 'fade',
+	pause: 1,
+	prev: '#prev',
+	next: '#next'
+});
+
+$("#slideshow").hover(function() {
+	$("ul#slide_nav").fadeIn();
+	},
+		function() {
+	$("ul#slide_nav").fadeOut();
+	});
+});
