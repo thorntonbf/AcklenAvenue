86efb7eb884cb03941ee78593b572f8eda199e37
Matt Perkins <ruiml3@gmail.com>
2016-09-19 23:19:05 -0500

    Add lean modal for alert message.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,22 @@
 $(document).ready(function() {
-$("#slideshow").css("overflow", "hidden");
+	$("#slideshow").css("overflow", "hidden");
 
-$("ul#slides").cycle({
-	fx: 'fade',
-	pause: 1,
-	prev: '#prev',
-	next: '#next'
-});
-
-$("#slideshow").hover(function() {
-	$("ul#slide_nav").fadeIn();
-	},
-		function() {
-	$("ul#slide_nav").fadeOut();
+	$("ul#slides").cycle({
+		fx: 'fade',
+		pause: 1,
+		prev: '#prev',
+		next: '#next'
 	});
+
+	$("#slideshow").hover(
+	function() {
+		$("ul#slide_nav").fadeIn();
+	},
+	function() {
+		$("ul#slide_nav").fadeOut();
+	});
+
+	$("body").leanModal({ top : 200, overlay : 0.4, closeButton: ".modal_close" });
+	$("body").trigger("click");
+	$("body").off("click");
 });
