cddec5805420374dfd756d0bcc0e2d89d8a9df5b
Matt Perkins <ruiml3@gmail.com>
2016-12-19 22:59:39 -0600

    modal for holiday 2016

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
+	var $message = $('#la-message'),
+		dayToStopShowing = new Date(2000,1,1),
+		today = new Date();
+
+	if ($message.length > 0) {
+		var year = $message.attr('data-year'),
+			day = $message.attr('data-day'),
+			month = $message.attr('data-month');
+
+		dayToStopShowing = new Date(day + " " + month + " " + year);
+	}
+
+	if (($message.length > 0) && (today < dayToStopShowing)) {
+		$("body").leanModal({ top : 200, overlay : 0.4, closeButton: ".modal_close" });
+		$("body").trigger("click");
+		$("body").off("click");
+	}
 
 	$("#slideshow").css("overflow", "hidden");
 
@@ -16,5 +33,4 @@ $(document).ready(function() {
 	function() {
 		$("ul#slide_nav").fadeOut();
 	});
-
 });
